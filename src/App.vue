d8f54b8ff1d25736b26d2c68d6aa75afd3ef9308
nromanr2d2@gmail.com <nromanr2d2@gmail.com>
2022-12-14 20:04:27 +0300

    Now UserCard component gets data from props

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App" :name="name" :photo="photo" :fio="fio" :adress="adress" :email="email" :phone="phone"/>
 </template>
 
 <script>
@@ -10,6 +10,15 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  one(){
+    photo: "";
+    name: "";
+    fio: "";
+    adress: "";
+    email: "";
+    phone: ""
+
   }
 }
 </script>
